a3b78d061b98984e36f86f3eb5f8227b62b9e285
Virtapatil2000 <virtapatil01@gmail.com>
2023-02-14 16:20:36 +0530

    [Virta] Adding Last Name

---
+++ UserRegistrationProblem/LastName.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class LastName
+    {
+        public static void LastN()
+        {
+            string pattern = "^[A-Z]{1}[A-Za-z]{2}$";
+            string[] input = { "ohk", "Pat", "11b", "Vbp", "ohk" };
+            foreach (var data in input)
+            {
+                Regex regexobj = new Regex(pattern);
+                if (regexobj.IsMatch(data))
+                {
+                    Console.WriteLine("{0} is Valid", data);
+                }
+                else
+                {
+                    Console.WriteLine("{0} is Invalid", data);
+                }
+            }
+        }
+    }
+}
--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -4,8 +4,24 @@
     {
         static void Main(string[] args)
         {
-           // Console.WriteLine("Hello, World!");
-            FirstName.FirstN();
+            Console.WriteLine("User Registration Problem");
+            Console.WriteLine("Enter the number");
+            Console.WriteLine("1.FirstName \n2.LastName \n9.exit");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            
+            switch (choice)
+            {
+                case 1:
+                    FirstName.FirstN();
+                    break;
+                case 2:
+                    LastName.LastN();
+                    break;
+                default:
+                    Console.WriteLine("Please Enter the option from Above");
+                    break;
+            }
+            Console.ReadLine();
         }
     }
 }
