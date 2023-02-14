939a83ddf1e14ae1d41da862ed7d349245a9eb5c
Virtapatil2000 <virtapatil01@gmail.com>
2023-02-14 16:03:44 +0530

    [Virta] Adding First Name

---
+++ UserRegistrationProblem/FirstName.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    internal class FirstName
+    {
+        public static void FirstN()
+        {
+            string pattern = "^[A-Z]{1}[A-Za-z]{2}$";
+            string[] input = { "Aaa", "Vir", "11", "vbp", "ohk" };
+            foreach(var data in input)
+            {
+                Regex regexobj= new Regex(pattern);
+                if(regexobj.IsMatch(data))
+                {
+                    Console.WriteLine("{0} is Valid", data);
+                }
+                else
+                { 
+                    Console.WriteLine("{0} is Invalid", data);
+                }
+            }
+        }
+    }
+}
